cc634ea5f724864593bb790607debf083d932043
Daniel Copley <djcopley@users.noreply.github.com>
2018-06-10 14:33:16 -0700

    Added Term class to keep track of whether terms are covered or not

--- minimize.py
+++ minimize.py
@@ -7,6 +7,37 @@ __author__ = 'Daniel Copley'
 __version__ = 'DEVELOPMENT'
 
 
+class Term():
+    """
+    Term object
+    """
+    def __init__(self, bin_str):
+        self.bin_str = bin_str
+        self.covered = False
+
+    def __eq__(self, other):
+        return True if self.bin_str == other.bin_str else False
+
+    def __hash__(self):
+        return hash(self.bin_str)
+
+    def __str__(self):
+        return self.bin_str
+
+    def __repr__(self):
+        return str(self)
+
+    def __iter__(self):
+        for bit in self.bin_str:
+            yield bit
+
+    def __getitem__(self, index):
+        return self.bin_str[index]
+
+    def __setitem__(self, index, value):
+        self.bin_str[index] = value
+
+
 def differ_by_one(nums):
     """
     :param nums: tuple of binary strings
@@ -17,9 +48,10 @@ def differ_by_one(nums):
     for index, bit in enumerate(num1):
         if bit != num2[index]:
             difference += 1
-        if difference > 1:
-            return False
-    return True if difference == 1 else False
+    if difference == 1:
+        num1.covered, num2.covered = True, True
+        return True
+    return False
 
 
 def reduce_bits(nums):
@@ -31,7 +63,7 @@ def reduce_bits(nums):
     result = ""
     for index, bit in enumerate(num1):
         result += bit if bit == num2[index] else '-'
-    return result
+    return Term(result)
 
 
 def get_pairs(terms):
@@ -54,17 +86,20 @@ def minimize(n_bits, minterms, xterms):
         raise ValueError("integer overflow")
 
     # Minimizing
-    minterms = [format(i, '0{}b'.format(n_bits)) for i in minterms]
-    xterms = [format(i, '0{}b'.format(n_bits)) for i in xterms]
+    minterms = [Term(format(i, '0{}b'.format(n_bits))) for i in minterms]
+    xterms = [Term(format(i, '0{}b'.format(n_bits))) for i in xterms]
     terms = set(minterms + xterms)
 
-    pairs = get_pairs(terms)
-    foo = filter(differ_by_one, pairs)
-    bar = map(reduce_bits, foo)
+    # The structure:
+    # 1. Get all combination pairs
+    # 2. Filter out items that differ by more than one bit
+    # 3. Go through each pair that differs by only one bit and create a new Term with covered bits dashed out
+    # 4. Repeat until there are no more simplifications
+    # 5. Find fully minimized equation using Petrick's method
 
-    print(list(bar))
-    # while True:
-    #     pass
+    pairs = get_pairs(terms)
+    foo = list(filter(differ_by_one, pairs))
+    bar = map(reduce_bits, foo)
 
 
 if __name__ == '__main__':
